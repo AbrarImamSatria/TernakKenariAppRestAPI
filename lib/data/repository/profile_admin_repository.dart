aad437d7fc546c0d7b7580428180aa3743cfcf0c
abrarimamsatria09 <092004abrar@gmail.com>
2025-06-05 12:25:24 +0700

    membuat ProfileAdminRepository

---
+++ lib/data/repository/profile_admin_repository.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:canary_app/data/models/request/admin/admin_profile_request.dart';
+import 'package:canary_app/data/models/response/admin_profile_response_model.dart';
+import 'package:canary_app/services/service_http_client.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class ProfileAdminRepository {
+  final ServiceHttpClient _serviceHttpClient;
+  final secureStorage = FlutterSecureStorage();
+
+  ProfileAdminRepository(this._serviceHttpClient);
+
+  Future<Either<String, AdminProfileResponseModel>> addProfile(
+    AdminProfileRequest requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.postWithToken(
+        'admin/profile',
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 201) {
+        final profileResponse = AdminProfileResponseModel.fromMap(jsonResponse);
+        log("Add Admin Profile successful: ${profileResponse.message}");
+        return Right(profileResponse);
+      } else {
+        log("Add Admin Profile failed: ${jsonResponse['message']}");
+        return Left(jsonResponse['message'] ?? "Create Profile failed");
+      }
+    } catch (e) {
+      log("Error in adding profile: $e");
+      return Left("An error occurred while adding profile: $e");
+    }
+  }
+}
